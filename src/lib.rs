b336bc8f588b062086e95dda1f84c68834d4de99
Martti Malmi <sirius@iki.fi>
2021-10-13 13:47:10 +0300

    wip websocket

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,20 @@ use std::sync::{
     Arc,
     RwLock
 };
+use wasm_bindgen::JsCast;
+use web_sys::{ErrorEvent, MessageEvent, WebSocket};
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -27,6 +41,8 @@ type SharedNodeStore = Arc<RwLock<HashMap<usize, Node>>>;
 // TODO use &str instead of String where possible
 // TODO proper automatic tests
 // TODO generic version for non-wasm usage
+// TODO break into submodules
+// TODO websocket
 
 #[wasm_bindgen]
 #[derive(Debug, Clone)]
@@ -41,10 +57,19 @@ pub struct Node {
     store: SharedNodeStore
 }
 
+fn random_string(len: usize) -> String {
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(len)
+        .map(char::from)
+        .collect()
+}
+
 #[wasm_bindgen]
 impl Node {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
+        let _ = Self::start_websocket();
         Self {
             id: 0,
             key: "".to_string(),
@@ -55,6 +80,94 @@ impl Node {
             map_subscriptions: Subscriptions::default(),
             store: SharedNodeStore::default()
         }
+    }
+
+    fn start_websocket() -> Result<(), JsValue> {
+        // Connect to an echo server
+        let ws = WebSocket::new("ws://localhost:8765/gun")?;
+        // For small binary messages, like CBOR, Arraybuffer is more efficient than Blob handling
+        ws.set_binary_type(web_sys::BinaryType::Arraybuffer);
+        // create callback
+        let cloned_ws = ws.clone();
+        let onmessage_callback = Closure::wrap(Box::new(move |e: MessageEvent| {
+            // Handle difference Text/Binary,...
+            if let Ok(abuf) = e.data().dyn_into::<js_sys::ArrayBuffer>() {
+                console_log!("message event, received arraybuffer: {:?}", abuf);
+                let array = js_sys::Uint8Array::new(&abuf);
+                let len = array.byte_length() as usize;
+                console_log!("Arraybuffer received {}bytes: {:?}", len, array.to_vec());
+                // here you can for example use Serde Deserialize decode the message
+                // for demo purposes we switch back to Blob-type and send off another binary message
+                cloned_ws.set_binary_type(web_sys::BinaryType::Blob);
+                match cloned_ws.send_with_u8_array(&vec![5, 6, 7, 8]) {
+                    Ok(_) => console_log!("binary message successfully sent"),
+                    Err(err) => console_log!("error sending message: {:?}", err),
+                }
+            } else if let Ok(blob) = e.data().dyn_into::<web_sys::Blob>() {
+                console_log!("message event, received blob: {:?}", blob);
+                // better alternative to juggling with FileReader is to use https://crates.io/crates/gloo-file
+                let fr = web_sys::FileReader::new().unwrap();
+                let fr_c = fr.clone();
+                // create onLoadEnd callback
+                let onloadend_cb = Closure::wrap(Box::new(move |_e: web_sys::ProgressEvent| {
+                    let array = js_sys::Uint8Array::new(&fr_c.result().unwrap());
+                    let len = array.byte_length() as usize;
+                    console_log!("Blob received {}bytes: {:?}", len, array.to_vec());
+                    // here you can for example use the received image/png data
+                })
+                    as Box<dyn FnMut(web_sys::ProgressEvent)>);
+                fr.set_onloadend(Some(onloadend_cb.as_ref().unchecked_ref()));
+                fr.read_as_array_buffer(&blob).expect("blob not readable");
+                onloadend_cb.forget();
+            } else if let Ok(txt) = e.data().dyn_into::<js_sys::JsString>() {
+                console_log!("received: {}", txt);
+                if let Ok(json) = js_sys::JSON::parse(&String::from(txt)) {
+                    console_log!("received json: {:?}", json);
+                }
+            } else {
+                console_log!("message event, received Unknown: {:?}", e.data());
+            }
+        }) as Box<dyn FnMut(MessageEvent)>);
+        // set message event handler on WebSocket
+        ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
+        // forget the callback to keep it alive
+        onmessage_callback.forget();
+
+        let onerror_callback = Closure::wrap(Box::new(move |e: ErrorEvent| {
+            console_log!("error event: {:?}", e);
+        }) as Box<dyn FnMut(ErrorEvent)>);
+        ws.set_onerror(Some(onerror_callback.as_ref().unchecked_ref()));
+        onerror_callback.forget();
+
+        let cloned_ws = ws.clone();
+        let onopen_callback = Closure::wrap(Box::new(move |_| {
+            console_log!("socket opened");
+            let msg_id = random_string(8);
+            let peer_id = random_string(8);
+            let m = format!("{{\"#\":\"{}\",\"dam\":\"hi\",\"pid\":\"{}\"}}", msg_id, peer_id);
+            match cloned_ws.send_with_str(&m) {
+                Ok(_) => console_log!("sent: {}", m),
+                Err(err) => console_log!("error sending message: {:?}", err),
+            }
+            let msg_id = random_string(8);
+            let val = random_string(36);
+            let time = js_sys::Date::now();
+            let m = format!("{{\"#\":\"{}\",\"put\":{{\"asdf\":{{\"_\":{{\"#\":\"asdf\",\">\":{{\"fasd\":{}}}}},\"fasd\":\"{}\"}}}}}}", msg_id, time, val);
+            match cloned_ws.send_with_str(&m) {
+                Ok(_) => console_log!("sent: {}", m),
+                Err(err) => console_log!("error sending message: {:?}", err),
+            }
+            let msg_id = random_string(8);
+            let m = format!("{{\"#\":\"{}\",\"get\":{{\"#\":\"asdf/fasd\",\".\":\"fasd\"}}}}", msg_id);
+            match cloned_ws.send_with_str(&m) {
+                Ok(_) => console_log!("sent: {}", m),
+                Err(err) => console_log!("error sending message: {:?}", err),
+            }
+        }) as Box<dyn FnMut(JsValue)>);
+        ws.set_onopen(Some(onopen_callback.as_ref().unchecked_ref()));
+        onopen_callback.forget();
+
+        Ok(())
     }
 
     fn new_child(&self, key: String) -> usize {
